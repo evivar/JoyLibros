3f6dfc7cbeafe47252d34f049fc4ef9bbd6fc5c5
evivar <evivar@ucm.es>
2020-07-05 23:17:52 +0200

    Terminada a falta de la foto
    Ya es responsive y solo falta la foto principal

--- js/app.js
+++ js/app.js
@@ -20,3 +20,19 @@ function sendEmail() {
     }, 2000);
 
 }
+
+$("[data-trigger]").on("click", function() {
+    var trigger_id = $(this).attr('data-trigger');
+    $(trigger_id).toggleClass("show");
+    $('body').toggleClass("offcanvas-active");
+});
+
+// close button 
+$(".btn-close").click(function(e) {
+    $(".navbar-collapse").removeClass("show");
+    $("body").removeClass("offcanvas-active");
+});
+
+$('.verResenyas').on("click", function() {
+    window.open('https://www.google.es/search?q=Compra+Venta+de+Libros+Ciudad+Lineal+JOY+LIBROS,+Calle+de+Alcal%C3%A1,+414,+28027+Madrid&ludocid=15094823321630282757#lrd=0xd422fe996c463d9:0xd17b95d5d10e1405,1');
+})
